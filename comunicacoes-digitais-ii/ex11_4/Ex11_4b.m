3618554a5097ad087e87592712c8f2f99a420e9d
Leandro Santana <leo93santana@gmail.com>
2018-05-09 16:29:16 -0300

    Update Ex11_4b.m

--- comunicacoes-digitais-ii/ex11_4/Ex11_4b.m
+++ comunicacoes-digitais-ii/ex11_4/Ex11_4b.m
@@ -1,1 +1,5 @@
-% MATLAB PROGRAM <Ex11_4b.m>% Este programa efetua a simulação de sistema CDMA para múltiplos usuários.% Os 4 usuarios têm potências diferentes para ilustrar o% efeito proximo-distante no caso de receptores convencionais de usuario único
+% MATLAB PROGRAM <Ex11_4b.m>
+% Este programa efetua a simulação de sistema CDMA para múltiplos usuários.
+% Os 4 usuarios têm potências diferentes para ilustrar o
+% efeito proximo-distante no caso de receptores convencionais de usuario único
+%
